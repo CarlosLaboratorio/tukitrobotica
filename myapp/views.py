eca36ace6eeb06c08f9b1811ee3fabd189a69e2b
Carlos Aguirre <carlosanibal815@ejemplo.com>
2025-05-01 17:41:11 -0300

    Refresh vistas clientes y vendedores

--- myapp/views.py
+++ myapp/views.py
@@ -32,11 +32,19 @@ def agregar_producto(request):
     return render(request, 'myapp/agregar_producto.html', {'form': form})
 
 def lista_clientes(request):
-    clientes = Clientes.objects.all()
+    query = request.GET.get('q')
+    if query:
+        clientes = Clientes.objects.filter(nombre__icontains=query) | Clientes.objects.filter(apellido__icontains=query)| Clientes.objects.filter(email__icontains=query)
+    else:
+        clientes = Clientes.objects.all()
     return render(request, 'myapp/clientes.html', {'clientes': clientes})
 
 def lista_vendedores(request):
-    vendedores = Vendedores.objects.all()
+    query = request.GET.get('q')
+    if query:
+        vendedores = Vendedores.objects.filter(nombre__icontains=query) | Vendedores.objects.filter(apellido__icontains=query)| Vendedores.objects.filter(email__icontains=query)
+    else:
+        vendedores = Vendedores.objects.all()
     return render(request, 'myapp/vendedores.html', {'vendedores': vendedores})
 
 def agregar_cliente(request):
