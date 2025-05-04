ecf922960a5dd5889c47c4857f31b9ec3a4aa901
Carlos Aguirre <carlosanibal815@ejemplo.com>
2025-05-04 15:10:38 -0300

    configurando pagina no encontrada

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@c##y7=pc660_b-m1%09pw@2=^ob5)r4gyqn$7#+(!^7zew90z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Proyecto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
